162dbb7f1243a815dd0cb48e0c5721722d44370d
Celeste Zapata <zdceleste@gmail.com>
2024-03-27 03:01:28 -0300

    Try fix GTM third party script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,11 +27,15 @@ class MyDocument extends Document {
             <script
               dangerouslySetInnerHTML={{
                 __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');`,
+                window.addEventListener('load', function() {
+                  var script = document.createElement('script');
+                  script.src = 'https://www.googletagmanager.com/gtm.js?id=${GOOGLE_TAG_MANAGER_ID}';
+                  script.async = true;
+                  script.onload = function() {
+                    console.log('GTM script loaded correctly, window.dataLayer has been populated');
+                  };
+                  document.head.appendChild(script);
+                });`,
               }}
             />
           )}
