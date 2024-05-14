1c74eab9f45c76d4dc78e6b83da1852fc796d804
Celeste Zapata <zdceleste@gmail.com>
2024-05-14 17:57:50 -0400

    Try fix ImageBySize, part 99999..

--- constants/imagePaths.ts
+++ constants/imagePaths.ts
@@ -3,6 +3,4 @@ import { ImageSet } from '@/types'
 export const HOME_PAGE_IMAGES: ImageSet = {
   name: 'home-page',
   description: 'Ilustration of a developer girl and her dog Luna.',
-  heigth: 364,
-  width: 767,
 }
